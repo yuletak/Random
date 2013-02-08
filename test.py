bccf7904eb78faa3c4b03f1a353ebe21030c8a2b
Yui Lee <yui@nttmcl.com>
2013-02-07 19:30:08 -0800

    Test on how to read input from test case file

--- test.py
+++ test.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env /usr/bin/python2.7
 
-from tcpdumplib import Tcpdump
-from constants import *
+from testlib.constants import *
+from testlib.tcpdumplib import Tcpdump
+from testlib.routemlib import Routem
 from time import sleep 
-from routemlib import Routem
 
-tcpdumpParams = {
-            CONNECT:  '/usr/bin/ssh',
-            USER:  'mcladmin',
-            HOST:  '216.69.72.141',
-            PWD:  'xyz123',
-            WAIT:  5
-         }
+params = execfile('/home/yui/CODE/github/Random/testcases/moduleA/tc-00-00-01')
 
-dump = Tcpdump(tcpdumpParams)
-route = Routem(tcpdumpParams)
-dump.start()
-route.execute()
-sleep(5)
-dump.stop()
-
+print params
 
