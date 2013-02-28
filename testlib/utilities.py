5b20c47cb66cacdf4e04493ccdcee2c277ff6711
Yui Lee <yui@nttmcl.com>
2013-02-27 19:41:19 -0800

    Parser for Jenkins CLI arguments

--- testlib/utilities.py
+++ testlib/utilities.py
@@ -12,3 +12,23 @@ def create_pexpect_obj(cmd, user, host, pwd):
     pObj.expect('password:')
     pObj.sendline(pwd)
     return pObj
+
+def parse_jenkins_argv(args):
+    count = len(args)
+    index = 1
+    var = {}
+    while arg < count:
+        if args[arg] == '-bnumber':
+            var[BUILDNUM] = args[arg+1]
+        if args[arg] == '-bid':
+            var[BUILDID] = args[arg+1]
+        if args[arg] == '-burl':
+            var[BUILDURL] = args[arg+1]
+        if args[arg] == '-btag':
+            var[BUILDID] = args[arg+1]
+        if args[arg] == '-commit':
+            var[GITCOMMIT] = args[arg+1]
+        if args[arg] == '-branch':
+            var[GITBRANCH] = args[arg+1]
+    return var 
+    
