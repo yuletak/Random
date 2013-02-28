9d90669c14cdee5f1f09ef8bd1f20bfa5d9d31dd
Yui Lee <yui@nttmcl.com>
2013-02-27 19:39:50 -0800

    moduleA test script
    moduleA.conf.py - new configuration info
    moduleA.py - test runner

--- tests/moduleA/moduleA.conf.py
+++ tests/moduleA/moduleA.conf.py
@@ -1,1 +1,10 @@
+VOHOST = 'https://www15.v1host.com/NTTMCL'
+VOUSER = 'yui'
+VOPWD = 'v3rs!0n0n3'
+VOSCOPENAME = 'moduleA'
+VOSCOPEID = '2785'
 
+VOSPRINTNAME = 'moduleA - Sprint 1'
+VOSPRINTID = '2839'
+
+VO
---
+++ tests/moduleA/moduleA.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# should be a better way to include the library code path
+from sys import path as path
+path += ['../../testlib']
+
+from constants import * 
+
+
+
+
+# parse configuration file
+# parse CLI input arguments
