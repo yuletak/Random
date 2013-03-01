6548d2faac3e7920523e7e0de1ff20daa2dd771f
Yui Lee <yui@nttmcl.com>
2013-02-28 19:58:39 -0800

    Done up to execution of dummy TC
    1) From conf file, get necessary VO info
    2) Connect to VO
    3) Retrieve TC number (VO naming convention)  based on sprint
    4) Get actual parameters from file in $INSTALL_DIR/testcases/$MODULE
    5) Create test object
    6) "execute" test case
    
    TODO:
        Error checking/handling
        Update info
        Defect reporting
        Regression testing
        Test sets?

--- tests/moduleA/moduleA.py
+++ tests/moduleA/moduleA.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
 
 # should be a better way to include the library code path
-from sys import path as path
-path += ['../../testlib']
+path += ['./testlib']
 
+from sys import path as path, argv as argv
+from utilities import parse_jenkins_argv as pja
+from utilities import get_testcase_param as gtp
+from utilities import get_vo_req as gvr
 from constants import * 
+from moduleAconf import *
+import urllib2, base64
+import xml.etree.ElementTree as ET
+from testcase import Testcase
 
+jEnv = {}
+jEnv = pja(argv)
 
+VOTestReq = gvr(VOUSER, VOPWD, VOTESTS)
+try:
+    VOTests = urllib2.urlopen(VOTestReq)
+    VOTestAssets = ET.fromstring(VOTests.read())
+except error:
+    print 'nada!'
 
+for test in VOTestAssets.findall('Asset'):
+    # Get test case file name; called "Number" in VO
+    tcFile = ''
+    for attrib in test.findall('Attribute'):
+        name = attrib.get('name')
+        if name == 'Number':
+            tcFile = attrib.text
+            break
+    if tcFile == '':
+        raise
+    tcInput = gtp(tcFile)
+    tcParam = tcInput[PARAM]
+    testcase = Testcase(tcParam)
+    code, mesg = testcase.execute()
+    print '{0}:  {1}'.format(code, mesg)
 
-# parse configuration file
-# parse CLI input arguments
+    # update the info in VO
+
