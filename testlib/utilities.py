90fd38916bf6bb77cf2127718983b63a6b144b3f
Yui Lee <yui@nttmcl.com>
2013-02-28 20:03:14 -0800

    Added common functions
    1) Parse python command line arguments (from Jenkins)
    2) Get urllib2 request object for VO API calls
    3) Read test case file input

--- testlib/utilities.py
+++ testlib/utilities.py
@@ -2,6 +2,7 @@
 
 from pexpect import spawn   
 from constants import *
+import urllib2, base64
 
 def create_pexpect_obj(cmd, user, host, pwd):
     connect = cmd + SPACE + user + '@' + host
@@ -17,18 +18,36 @@ def parse_jenkins_argv(args):
     count = len(args)
     index = 1
     var = {}
-    while arg < count:
-        if args[arg] == '-bnumber':
-            var[BUILDNUM] = args[arg+1]
-        if args[arg] == '-bid':
-            var[BUILDID] = args[arg+1]
-        if args[arg] == '-burl':
-            var[BUILDURL] = args[arg+1]
-        if args[arg] == '-btag':
-            var[BUILDID] = args[arg+1]
-        if args[arg] == '-commit':
-            var[GITCOMMIT] = args[arg+1]
-        if args[arg] == '-branch':
-            var[GITBRANCH] = args[arg+1]
+    while index < count:
+        if args[index] == '-bnumber':
+            var[JBUILDNUM] = args[index+1]
+        if args[index] == '-bid':
+            var[JBUILDID] = args[index+1]
+        if args[index] == '-burl':
+            var[JBUILDURL] = args[index+1]
+        if args[index] == '-btag':
+            var[JBUILDID] = args[index+1]
+        if args[index] == '-commit':
+            var[JGITCOMMIT] = args[index+1]
+        if args[index] == '-branch':
+            var[JGITBRANCH] = args[index+1]
+        index += 1
     return var 
-    
+
+def get_vo_req(USER, PWD, URL):
+    base64string = base64.standard_b64encode('%s:%s' % (USER, PWD))
+    req = urllib2.Request(URL)
+    req.add_header("Authorization", "Basic %s" % base64string)
+    return req
+
+def get_testcase_param(testcase):
+    if testcase == None:
+        raise
+    f = open('./testcases/moduleA/' + testcase, 'r')
+    lines = f.read()
+    mystr = ''
+    for line in lines:
+        mystr = mystr + line.strip('\t\n')
+    f.close()
+    params = eval(mystr)
+    return params
